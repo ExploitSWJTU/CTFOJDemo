2e0356f946a03e06a21be6342afc03a6ad69140e
Weedy233 <HY_Tu_Zhi_233@outlook.com>
2026-02-15 16:31:15 +0800

    feat: add eslint-plugin-unused-imports to manage unused imports in the project

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,17 @@ import pluginVue from 'eslint-plugin-vue';
 import tseslint from 'typescript-eslint';
 import pluginJs from '@eslint/js';
 import globals from 'globals';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 export default tseslint.config(
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...pluginVue.configs['flat/recommended'],
+  {
+    plugins: {
+      'unused-imports': unusedImports,
+    },
+  },
   {
     files: ['*.vue', '**/*.vue', '**/*.ts', '**/*.js'],
     languageOptions: {
@@ -25,7 +31,21 @@ export default tseslint.config(
     rules: {
       'vue/multi-word-component-names': 'off',
       '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-unused-vars': 'warn',
+
+      // 自动删除未使用的导入，但保留未使用的变量报错用于手动处理
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'error',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+
       'no-useless-assignment': 'off',
       'vue/max-attributes-per-line': [
         'error',
